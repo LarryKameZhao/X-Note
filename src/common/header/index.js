45e9cac661ff869052c1d640fdd0c553a36db400
LarryKameZhao <larryzhao1994@outlook.com>
2018-09-28 23:09:51 +0800

    'data-from-axios'

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {CSSTransition} from 'react-transition-group'
 import { connect } from 'react-redux';
 import  {actionCreators}    from  './store'
@@ -18,83 +18,87 @@ import {
   SearchInfoItem
 } from "./style";
 
-const getListArea = (show) => {
-  if (show) {
+class Header extends  Component {
+  render() {
     return (
-      <SearchInfo>
-        <SearchInfoTitle>
-          热门搜索
-          <SearchInfoSwitch>换一换</SearchInfoSwitch>
-        </SearchInfoTitle>
-        <SearchInfoList>
-          <SearchInfoItem>区块链</SearchInfoItem>
-          <SearchInfoItem>小程序</SearchInfoItem>
-          <SearchInfoItem>Vue</SearchInfoItem>
-          <SearchInfoItem>PHP</SearchInfoItem>
-          <SearchInfoItem>故事</SearchInfoItem>
-        </SearchInfoList>
-      </SearchInfo>
+      <HeaderWrapper>
+        <Logo/>
+        <Nav>
+          <NavItem className='left active'>首页</NavItem>
+          <NavItem className='left'>下载App</NavItem>
+          <NavItem className='right'>登陆</NavItem>
+          <NavItem className='right'>
+            <svg className="icon" aria-hidden="true">
+              <use xlinkHref="#icon-Aa"></use>
+            </svg>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={this.props.focused}
+              timeout={300}
+              classNames="slide"
+            >
+              <NavSearch
+                className={this.props.focused ? 'focused' : ''}
+                onFocus={this.props.handleInputFocus}
+                onBlur={this.props.handleInputBlur}
+              ></NavSearch>
+            </CSSTransition>
+            <svg className={this.props.focused ? 'icon focused' : 'icon'} aria-hidden="true">
+              <use xlinkHref="#icon-search"></use>
+            </svg>
+            {this.getListArea(this.props.focused)}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className='writing'>
+            <svg className="icon" aria-hidden="true">
+              <use xlinkHref="#icon-pencil2"></use>
+            </svg>
+            写文章
+          </Button>
+          <Button className='reg'>注册</Button>
+        </Addition>
+      </HeaderWrapper>
     )
   }
-  else {
-    return null
+  getListArea = () => {
+    if (this.props.focused) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一换</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            {
+              this.props.list.map((item) => {
+                return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+              })
+
+            }
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    }
+    else {
+      return null
+    }
   }
 }
 
-const Header = (props) => {
-  return (
-    <HeaderWrapper>
-      <Logo/>
-      <Nav>
-        <NavItem className='left active'>首页</NavItem>
-        <NavItem className='left'>下载App</NavItem>
-        <NavItem className='right'>登陆</NavItem>
-        <NavItem className='right'>
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-Aa"></use>
-          </svg>
-        </NavItem>
-        <SearchWrapper>
-          <CSSTransition
-            in={props.focused}
-            timeout={300}
-            classNames="slide"
-          >
-            <NavSearch
-              className = {props.focused ?'focused':''}
-              onFocus = {props.handleInputFocus}
-              onBlur={props.handleInputBlur}
-            ></NavSearch>
-          </CSSTransition>
-          <svg className = {props.focused ?'icon focused':'icon'} aria-hidden="true">
-            <use xlinkHref="#icon-search"></use>
-          </svg>
-          { getListArea(props.focused)}
-        </SearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className='writing'>
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-pencil2"></use>
-          </svg>写文章
-        </Button>
-        <Button className='reg'>注册</Button>
-      </Addition>
-    </HeaderWrapper>
-    )
-
-}
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.get('header').get('focused')
+    focused: state.get('header').get('focused'),
+    list: state.getIn(['header','list'])
   }
 }
 const mapDispatchToProps = (dispatch) => {
   return {
     handleInputFocus () {
-      const action = actionCreators.searchFocus()
-      dispatch(action)
+      dispatch(actionCreators.searchFocus())
+      dispatch(actionCreators.getList())
     },
     handleInputBlur () {
       const action = actionCreators.searchBlur()
--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -1,4 +1,6 @@
 import * as actionTypes from './constants'
+import axios from 'axios'
+import { fromJS } from 'immutable'
 export const searchFocus = () => ({
   type: actionTypes.SEARCH_FOCUS
 })
@@ -6,3 +8,16 @@ export const searchFocus = () => ({
 export const searchBlur = () => ({
   type: actionTypes.SEARCH_BLUR
 })
+export const changeList = (data) => ({
+  type: actionTypes.CHANGE_LIST,
+  data: fromJS(data)
+})
+export const getList = () => {
+  return (dispatch) => {
+    axios.get('/api/headerList.json').then((res)=>{
+      dispatch(changeList(res.data.data))
+    }).catch(()=>{
+      console.log('error')
+    })
+  }
+}
