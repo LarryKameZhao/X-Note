c4ffb3bf255d590d9dd05a43a8914ca5f69529d5
LarryKameZhao <larryzhao1994@outlook.com>
2018-09-28 14:58:47 +0800

    'finish-header-redux-init'

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -11,55 +11,49 @@ import {
   Button,
   SearchWrapper
 } from "./style";
-
-class Header extends Component {
-  constructor (props) {
-    super(props)
-  }
-  render () {
-    return (
-      <HeaderWrapper>
-        <Logo/>
-        <Nav>
-          <NavItem className='left active'>首页</NavItem>
-          <NavItem className='left'>下载App</NavItem>
-          <NavItem className='right'>登陆</NavItem>
-          <NavItem className='right'>
-            <svg className="icon" aria-hidden="true">
-              <use xlinkHref="#icon-Aa"></use>
-            </svg>
-          </NavItem>
-          <SearchWrapper>
-            <CSSTransition
-              in={this.props.focused}
-              timeout={300}
-              classNames="slide"
-            >
-             <NavSearch
-              className = {this.props.focused ?'focused':''}
-              onFocus = {this.props.handleInputFocus}
-              onBlur={this.props.handleInputBlur}
-              ></NavSearch>
-            </CSSTransition>
-              <svg className = {this.props.focused ?'icon focused':'icon'} aria-hidden="true">
-                <use xlinkHref="#icon-search"></use>
-              </svg>
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Button className='writing'>
-            <svg className="icon" aria-hidden="true">
-              <use xlinkHref="#icon-pencil2"></use>
-            </svg>写文章
-          </Button>
-          <Button className='reg'>注册</Button>
-        </Addition>
-      </HeaderWrapper>
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo/>
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载App</NavItem>
+        <NavItem className='right'>登陆</NavItem>
+        <NavItem className='right'>
+          <svg className="icon" aria-hidden="true">
+            <use xlinkHref="#icon-Aa"></use>
+          </svg>
+        </NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            in={props.focused}
+            timeout={300}
+            classNames="slide"
+          >
+            <NavSearch
+              className = {props.focused ?'focused':''}
+              onFocus = {props.handleInputFocus}
+              onBlur={props.handleInputBlur}
+            ></NavSearch>
+          </CSSTransition>
+          <svg className = {props.focused ?'icon focused':'icon'} aria-hidden="true">
+            <use xlinkHref="#icon-search"></use>
+          </svg>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className='writing'>
+          <svg className="icon" aria-hidden="true">
+            <use xlinkHref="#icon-pencil2"></use>
+          </svg>写文章
+        </Button>
+        <Button className='reg'>注册</Button>
+      </Addition>
+    </HeaderWrapper>
     )
-  }
-
 
 }
+
 const mapStateToProps = (state) => {
   return {
     focused: state.focused
@@ -68,10 +62,16 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     handleInputFocus () {
-      console.log('focus')
+      const action = {
+        type: 'search_focus'
+      }
+      dispatch(action)
     },
     handleInputBlur () {
-      console.log('blur')
+      const action = {
+        type: 'search_blur'
+      }
+      dispatch(action)
     }
   }
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,5 +3,15 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
+  if (action.type === 'search_focus') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.focused = true
+    return newState
+  }
+  if (action.type === 'search_blur') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.focused = false
+    return newState
+  }
   return state
 }
